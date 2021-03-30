a483f2cbd3d3a8a5bbca1c1f88e8dba8cebad8d6
jxm <595611250@qq.com>
2021-03-31 00:11:17 +0800

    webpack 配置和loader

--- LearnVuejs05-v2/01-webpack的使用/02-webpack的配置/webpack.config.js
+++ LearnVuejs05-v2/01-webpack的使用/02-webpack的配置/webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/main.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
+  entry: './src/main.js',    //指定入口
+  output: {                //对应的出口
+    path: path.resolve(__dirname, 'dist'),  //__dirname：node中自带的全局变量
     filename: 'bundle.js'
   },
 }
