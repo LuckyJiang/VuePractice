f854a44ead1a9d90db4210559adf6448b0661138
jxm <595611250@qq.com>
2021-04-01 00:12:25 +0800

    webpack配置分离

--- LearnVuejs05-v2/01-webpack的使用/06-webpack配置分离/build/dev.config.js
+++ LearnVuejs05-v2/01-webpack的使用/06-webpack配置分离/build/dev.config.js
@@ -1,6 +1,10 @@
 const webpackMerge = require('webpack-merge')
 const baseConfig = require('./base.config')
 
+/**
+ * 只在开发阶段会用到的配置
+ * @type {function(...[*]=)}
+ */
 module.exports = webpackMerge(baseConfig, {
   devServer: {
     contentBase: './dist',
--- LearnVuejs05-v2/01-webpack的使用/06-webpack配置分离/build/prod.config.js
+++ LearnVuejs05-v2/01-webpack的使用/06-webpack配置分离/build/prod.config.js
@@ -2,6 +2,10 @@ const UglifyjsWebpackPlugin = require('uglifyjs-webpack-plugin')
 const webpackMerge = require('webpack-merge')
 const baseConfig = require('./base.config')
 
+/**
+ * 发布之后的配置
+ * @type {function(...[*]=)}
+ */
 module.exports = webpackMerge(baseConfig, {
   plugins: [
     new UglifyjsWebpackPlugin()
