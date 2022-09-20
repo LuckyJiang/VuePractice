bec047ac1105227a84507ab90c5b5733aa85278b
jiangxiaomin <jxm123>
2022-09-20 14:00:02 +0800

    import 别名

--- LearnVuejs07/03-tabbar/src/main.js
+++ LearnVuejs07/03-tabbar/src/main.js
@@ -15,18 +15,19 @@ new Vue({
  *
  *
  * 2、引入文件：
- *    2.1、在script标签中引用文件：@import
- *        方式1：
+ *    2.1、引用方式：
+ *        方式1：在style标签中引用文件,@import
  *         <style>
                @import "./assets/css/base.css";
             </style>
 
-          方式2：import MainTabBar from './components/mainTabbar/MainTabBar'
+          方式2：在script标签中引用文件,import
+          import MainTabBar from './components/mainTabbar/MainTabBar'
  *
  * 3、关于slot插槽设置属性：<div v-if="!isActive"><slot name="item-icon"></slot></div>
  *    建议slot被一个div包围，然后所有的属性设置和事件绑定都写在div中，因为后期slot会被外部的东西全部替换。
  *
- * 4、 给文件夹起别名，这个是webpack的配置，应该在webpack中进行修改：
+ * 4、 给文件夹起别名:这个是webpack的配置，应该在webpack中进行修改：
  *    （1）：在文件夹build下：
  *    resolve: {
         //导入文件如果包含以下三个文件后缀的扩展，则表示可是将其省略
@@ -41,7 +42,7 @@ new Vue({
       }
  *
  *    （2）如何使用别名：
- *    通过import引入组件则可以直接使用别名，比如：@
- *    如果不是通过import的形式引入的，则需要在别名前加~，比如：~@
+ *    通过import引入组件则可以直接使用别名，比如：@、components
+ *    如果不是通过import的形式引入的，则需要在别名前加~，比如：~@、~components
  *
  */
