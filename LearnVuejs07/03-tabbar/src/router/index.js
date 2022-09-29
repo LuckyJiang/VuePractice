c5e47ee5f891da0fc8efc640f73800b843e5a306
jiangxiaomin <jxm123>
2022-09-29 10:35:27 +0800

    echarts 绘制曲线图

--- LearnVuejs07/03-tabbar/src/router/index.js
+++ LearnVuejs07/03-tabbar/src/router/index.js
@@ -6,6 +6,7 @@ const Home = () => import('../views/home/Home')
 const Category = () => import('../views/category/Category')
 const Cart = () => import('../views/cart/Cart')
 const Profile = () => import('../views/profile/Profile')
+const MyEcharts = () => import('../views/echarts/MyEcharts')
 
 // 1.安装插件
 Vue.use(VueRouter)
@@ -31,6 +32,9 @@ const routes = [
   {
     path: '/profile',
     component: Profile
+  },{
+    path: '/echarts',
+    component: MyEcharts
   }
 ]
 const router = new VueRouter({
