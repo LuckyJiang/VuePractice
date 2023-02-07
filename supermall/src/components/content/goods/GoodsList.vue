f002a5cdad97b614f77f96c1a9bcb944f4177db4
jiangxiaomin <jxm123>
2023-02-07 13:47:09 +0800

    this.$emit  与 this.$bus.$emit 的区别

--- supermall/src/components/content/goods/GoodsList.vue
+++ supermall/src/components/content/goods/GoodsList.vue
@@ -5,7 +5,10 @@
 </template>
 
 <script>
-  import GoodsListItem from './GoodsListItem'
+/**
+ * 该组件为商品列表展示组件，在多个页面中会被使用，且与业务相关，故放在components/content组件中。
+ */
+import GoodsListItem from './GoodsListItem'
 
   export default {
     name: "GoodsList",
@@ -26,8 +29,8 @@
 <style scoped>
   .goods {
     display: flex;
-    flex-wrap: wrap;
-    justify-content: space-around;
+    flex-wrap: wrap; /* flex + wrap，根据子元素的固定长度，来决定当前元素每行显示几个子元素*/
+    justify-content: space-around; /* space-around，将每行的子元素均等分*/
 
     padding: 2px;
   }
