9279bb7dcc7d4b909c18de1305e7c126cf9571f7
jiangxiaomin <jxm123>
2024-08-06 09:25:19 +0800

    0805学习

--- LearnVuejs04/08-书籍购物车案例/高阶函数.js
+++ LearnVuejs04/08-书籍购物车案例/高阶函数.js
@@ -56,28 +56,3 @@ console.log(total);
 // 第二次: preValue 60 n 80
 // 第二次: preValue 140 n 100
 // 240
-
-// // 1.需求: 取出所有小于100的数字
-// let newNums = []
-// for (let n of nums) {
-//   if (n < 100) {
-//     newNums.push(n)
-//   }
-// }
-//
-// // 2.需求:将所有小于100的数字进行转化: 全部*2
-// let new2Nums = []
-// for (let n of newNums) {
-//   new2Nums.push(n * 2)
-// }
-//
-// console.log(new2Nums);
-//
-//
-// // 3.需求:将所有new2Nums数字相加,得到最终的记过
-// let total = 0
-// for (let n of new2Nums) {
-//   total += n
-// }
-//
-// console.log(total);
